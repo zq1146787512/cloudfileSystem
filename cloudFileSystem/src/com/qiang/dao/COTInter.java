746a684d098a5ddf35713243e349833c059291f6
zq1146787512 <1146787512@qq.com>
2017-02-28 14:42:59 +0800

    Signed-off-by: zq1146787512 <1146787512@qq.com>

--- cloudFileSystem/src/com/qiang/dao/COTInter.java
+++ cloudFileSystem/src/com/qiang/dao/COTInter.java
@@ -1,15 +1,16 @@
 package com.qiang.dao;
 
-import com.qiang.dto.File;
 import com.qiang.dto.Operation;
 /**
  *操作转换的接口
  */
 public interface COTInter {
-	//返回路径关系的接口
+	public  Operation getOperation();
+	public  void deleteByPrimaryKey();
+	/*//返回路径关系的接口
 	String PathRealtion(File NI,File NJ);
 	//由路径关系判断的COT转换函数,由操作的类型来判断选取的转换函数
-	Operation TR(Operation c1,Operation c2);
+	Operation TR(Operation c1,Operation c2);*/
 	
 
 }
